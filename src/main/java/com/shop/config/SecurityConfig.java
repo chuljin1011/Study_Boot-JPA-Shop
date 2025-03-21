a4ae2170539e2852649ac23d3a634dc0156cdd8e
JIN <jini7592@naver.com>
2025-03-21 22:15:03 +0900

    ver.03 - 회원가입, 로그인/로그아웃

---
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.shop.config;
+
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    MemberService memberService;
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/");
+
+        http.authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                new AntPathRequestMatcher("/"),
+                new AntPathRequestMatcher("/members/**"),
+                new AntPathRequestMatcher("/item/**"),
+                new AntPathRequestMatcher("/images/**"),
+                new AntPathRequestMatcher("/css/**"),
+                new AntPathRequestMatcher("/js/**"),
+                new AntPathRequestMatcher("/img/**"))
+            .permitAll()
+            .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasRole("ADMIN")
+            .anyRequest().authenticated());
+
+        http.exceptionHandling(ex -> ex
+                .accessDeniedPage("/access-denied")
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
