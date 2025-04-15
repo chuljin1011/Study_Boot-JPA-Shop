f8868f01840bdc81bea6266cef68bbec0f4b233c
JIN <jini7592@naver.com>
2025-04-15 17:56:46 +0900

    ver.06 - 상품 등록 및 조회

---
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -0,0 +1,43 @@
+package com.shop.service;
+
+import com.shop.entity.ItemImg;
+import com.shop.repository.ItemImgRepository;
+import com.shop.repository.ItemRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.StringUtils;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ItemImgService {
+
+    private final ItemImgRepository itemImgRepository;
+    @Value("${itemImgLocation}")
+    private String itemImgLocation;
+
+    private final ItemRepository itemRepository;
+
+    private final FileService fileService;
+
+    public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile) throws Exception {
+        String oriImgName = itemImgFile.getOriginalFilename();
+        String imgName = "";
+        String imgUrl = "";
+
+        //파일 업로드
+        if(!StringUtils.isEmpty(oriImgName)) {
+            imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            imgUrl = "/images/item" + imgName;
+        }
+
+        //상품 이미지 정보 저장
+        itemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        itemImgRepository.save(itemImg);
+    }
+
+
+}
---
+++ src/main/java/com/shop/service/ItemService.java
@@ -0,0 +1,30 @@
+package com.shop.service;
+
+import com.shop.dto.ItemFormDto;
+import com.shop.entity.Item;
+import com.shop.repository.ItemImgRepository;
+import com.shop.repository.ItemRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+    private final ItemImgService itemImgService;
+    private final ItemImgRepository itemImgRepository;
+
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception {
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+
+    }
+
+}
