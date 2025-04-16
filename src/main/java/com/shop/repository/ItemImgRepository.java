09d7106a13f34ce0e53012b1e9e7732ca8a61d6e
JIN <jini7592@naver.com>
2025-04-16 17:51:02 +0900

    ver.06 - 상품 등록 및 조회 ( 상품 수정 )

--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -3,5 +3,11 @@ package com.shop.repository;
 import com.shop.entity.ItemImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemImgRepository extends JpaRepository<ItemImg, Integer> {
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+    List<ItemImg> findByItemIdOrderByIdAsc(Long id);
+
 }
--- src/main/java/com/shop/service/ItemImgService.java
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -3,6 +3,7 @@ package com.shop.service;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,6 +38,26 @@ public class ItemImgService {
         //상품 이미지 정보 저장
         itemImg.updateItemImg(oriImgName, imgName, imgUrl);
         itemImgRepository.save(itemImg);
+    }
+
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) throws Exception {
+        if(!itemImgFile.isEmpty()){
+            ItemImg savedItemImg = itemImgRepository.findById(itemImgId)
+                    .orElseThrow(EntityNotFoundException::new);
+
+            String imgUrl = savedItemImg.getImgUrl();
+
+            //기존 이미지 파일 삭제 - 기존 이미지 삭제 안됨. 이후 보수 필요.
+            if(!StringUtils.isEmpty(savedItemImg.getImgName())) {
+                fileService.deleteFile(imgUrl);
+            }
+
+            String oriImgName = itemImgFile.getOriginalFilename();
+            String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            imgUrl = "/images/item/" + imgName;
+
+            savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        }
     }
 
 
