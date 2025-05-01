c0d3069c8f5c09d6496ff6450ac50a66f1e80ef0
JIN <jini7592@naver.com>
2025-05-01 19:46:06 +0900

    ver.07 - 주문 (초기작업)

--- src/main/java/com/shop/controller/OrderController.java
+++ src/main/java/com/shop/controller/OrderController.java
@@ -1,12 +1,32 @@
 package com.shop.controller;
 
 
+import com.shop.dto.OrderDto;
+import com.shop.service.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderService orderService;
+
+    @PostMapping(value = "/order")
+    public @ResponseBody ResponseEntity order (@RequestBody @Valid OrderDto orderDto, BindingResult bindingResult, Principal principal) {
+
+        if(bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+        }
+
+    }
 
 }
